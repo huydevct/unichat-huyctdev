56273b5c2023cf7a6af47e260ebebcedeed7a595
huyctdev <huyvipct1@gmail.com>
2021-08-14 17:01:55 +0700

    finished and deployed

---
+++ src/components/Chats.js
@@ -0,0 +1,83 @@
+import React, { useRef, useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { auth } from "../firebase";
+import { ChatEngine } from "react-chat-engine";
+
+import { useAuth } from "../contexts/AuthContext";
+import axios from "axios";
+
+const Chats = () => {
+  const history = useHistory();
+  const { user } = useAuth();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (!user) {
+      history.push("/");
+
+      return;
+    }
+
+    axios.get("https://api.chatengine.io/users/me", {
+        headers: {
+          "project-id": process.env.REACT_APP_CHAT_ENGINE_ID,
+          "user-name": user.email,
+          "user-secret": user.uid,
+        },
+      })
+      .then(() => {
+        setLoading(false);
+      })
+      .catch(() => {
+        let formData = new FormData();
+        formData.append("email", user.email);
+        formData.append("username", user.email);
+        formData.append("secret", user.uid);
+
+        getFile(user.photoURL)
+        .then((avatar) => {
+          formData.append("avatar", avatar, avatar.name);
+
+          axios.post("https://api.chatengine.io/users/", formData, {
+            headers: { "private-key": process.env.REACT_APP_CHAT_ENGINE_KEY },
+          })
+          .then(() => setLoading(false))
+          .catch((err) => console.log(err))
+        });
+      });
+  }, [user, history]);
+
+  const handleLogout = async () => {
+    await auth.signOut();
+    history.push("/");
+  };
+
+  const getFile = async (url) => {
+    const response = await fetch(url);
+    const data = await response.blob();
+
+    return new File([data], "userPhoto.jpg", { type: "image/jpeg" });
+  };
+
+  if(!user || loading) return 'Loading...';
+
+  return (
+    <div className="chats-page">
+      <div className="nav-bar">
+        <div className="logo-tab">Unichat</div>
+        <div className="logout-tab" onClick={handleLogout}>
+          Logout
+        </div>
+      </div>
+
+      <ChatEngine
+        height="calc(100vh-66px)"
+        projectID={process.env.REACT_APP_CHAT_ENGINE_ID}
+        userName={user.email}
+        userSecret={user.uid}
+      />
+    </div>
+  );
+};
+
+export default Chats;
--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -8,14 +8,14 @@ export const useAuth = () => useContext(AuthContext);
 
 export const AuthProvider = ({ children }) => {
   const [loading, setLoading] = useState(true);
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState(null);
   const history = useHistory();
 
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
       setUser(user);
       setLoading(false);
-      history.push("/chats");
+      if(user) history.push("/chats");
     });
   }, [user, history]);
 
